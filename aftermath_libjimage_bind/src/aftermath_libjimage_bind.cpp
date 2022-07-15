4dd12b4f0f8d2c5552a71899f635c342f811dc30
Gimgim <93856041+gimbles@users.noreply.github.com>
2022-07-16 00:03:36 +0530

    Add libfmt to aftermath_libjimage_bind, current state is borked.

--- aftermath_libjimage_bind/src/aftermath_libjimage_bind.cpp
+++ aftermath_libjimage_bind/src/aftermath_libjimage_bind.cpp
@@ -1,6 +1,8 @@
-#include "../include/aftermath_libjimage_bind.h"
-#include "../include/jimage.hpp"
+#include "aftermath_libjimage_bind.h"
+#include "jimage.hpp"
+#include <array>
 #include <cstring>
+#include <fmt/format.h>
 #include <iostream>
 #include <map>
 #include <vector>
@@ -11,13 +13,19 @@ bool ctw_visitor(JImageFile *jimage, const char *module_name,
                  const char *version, const char *package, const char *name,
                  const char *extension, void *arg)
 {
+
   if (strcmp(extension, "class") == 0) {
+
     jlong size;
-    auto location =
-        JIMAGE_FindResource(jimage, module_name, version, name, &size);
-    auto buffer = std::vector<char>();
-    JIMAGE_GetResource(jimage, location, buffer.data(), size);
-    classes.insert(std::string(package), buffer);
+    JImageLocationRef location = JIMAGE_FindResource(
+        jimage, "java.base", "9.0", "java/lang/String.class", &size);
+    auto buffer = new char[size];
+    auto length = JIMAGE_GetResource(jimage, location, buffer, size);
+    std::vector<char> vec_buffer(buffer, buffer + size);
+    std::cout << vec_buffer.size() << std::endl;
+    auto class_name = fmt::format("{}/{}.{}", package, name, extension);
+
+    classes.insert({class_name, vec_buffer});
   }
 
   return true;
@@ -30,5 +38,17 @@ int main(int argc, char **argv)
       "/usr/lib/jvm/java-17-openjdk-17.0.3.0.7-2.fc36.x86_64/lib/modules",
       &error_code);
   JIMAGE_ResourceIterator(value, ctw_visitor, nullptr);
+
+  if (error_code != 0) {
+    std::cerr << "Failure." << std::endl;
+    return 1;
+  } else {
+    for (const auto [key, value] : classes) {
+      std::cout << key << std::endl;
+
+      // std::cout << value.size() << std::endl;
+    }
+  }
+
   return 0;
 }
