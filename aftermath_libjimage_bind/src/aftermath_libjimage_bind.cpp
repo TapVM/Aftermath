9e472d3bc3d73a8baf1509560a07ae593245a7d4
Gimgim <93856041+gimbles@users.noreply.github.com>
2022-07-16 12:06:31 +0530

    refactor libjimage_bind

--- aftermath_libjimage_bind/src/aftermath_libjimage_bind.cpp
+++ aftermath_libjimage_bind/src/aftermath_libjimage_bind.cpp
@@ -1,29 +1,41 @@
-#include "aftermath_libjimage_bind.h"
-#include "jimage.hpp"
-#include <array>
-#include <cstring>
-#include <fmt/format.h>
-#include <iostream>
-#include <map>
-#include <vector>
+//-------------- libjimage_bind - Instruction class definition ---------------//
+//
+// Part of the Aftermath JVM, under the MIT license. The LICENSE file is present
+// at the project root, please consult to it for license information.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file is a thin wrapper for libjimage, providing Rust-friendly bindings.
+// It depends on
+// - libfmt (vendored in the `libfmt` directory of aftermath_libjimage_bind).
+//
+//===----------------------------------------------------------------------===//
 
+#include <aftermath_libjimage_bind.h> /* Exposing library methods             */
+#include <cstring>                    /* strcmp()                             */
+#include <fmt/format.h>               /* libfmt                               */
+#include <iostream>                   /* Logging                              */
+#include <jimage.hpp>                 /* Bindings to libjimage                */
+#include <map>                        /* std::map                             */
+#include <vector>                     /* std::vector                          */
+
+// SAFETY -> `classes` will *never* be mutated concurrently/in parallel.
+// All operations to classes will happen sequentially.
 static std::map<std::string, std::vector<char>> classes = {};
 
-bool ctw_visitor(JImageFile *jimage, const char *module_name,
-                 const char *version, const char *package, const char *name,
-                 const char *extension, void *arg)
+bool ctw_visitor(JImageFile *jimage, const char *_module_name,
+                 const char *_version, const char *package, const char *name,
+                 const char *extension, void *_arg)
 {
-
   if (strcmp(extension, "class") == 0) {
-
     jlong size;
-    JImageLocationRef location = JIMAGE_FindResource(
-        jimage, "java.base", "9.0", "java/lang/String.class", &size);
-    auto buffer = new char[size];
-    auto length = JIMAGE_GetResource(jimage, location, buffer, size);
-    std::vector<char> vec_buffer(buffer, buffer + size);
-    std::cout << vec_buffer.size() << std::endl;
     auto class_name = fmt::format("{}/{}.{}", package, name, extension);
+    JImageLocationRef location = JIMAGE_FindResource(
+        jimage, package, "9.0", "java/lang/String.class", &size);
+
+    char *buffer = new char[size];
+    JIMAGE_GetResource(jimage, location, buffer, size);
+    std::vector<char> vec_buffer(buffer, buffer + size);
 
     classes.insert({class_name, vec_buffer});
   }
@@ -31,7 +43,7 @@ bool ctw_visitor(JImageFile *jimage, const char *module_name,
   return true;
 }
 
-int main(int argc, char **argv)
+int main()
 {
   auto error_code = 0;
   auto value = JIMAGE_Open(
@@ -43,7 +55,7 @@ int main(int argc, char **argv)
     std::cerr << "Failure." << std::endl;
     return 1;
   } else {
-    for (const auto [key, value] : classes) {
+    for (const auto &[key, value] : classes) {
       std::cout << key << std::endl;
 
       // std::cout << value.size() << std::endl;
